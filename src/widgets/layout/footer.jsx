fd889f022774f9230bf12c95f092619a90bcb171
dembasow98 <dastech1998@gmail.com>
2023-04-30 20:00:48 +0300

    First commit for the project.

---
+++ src/widgets/layout/footer.jsx
@@ -0,0 +1,61 @@
+import PropTypes from "prop-types";
+import { Typography } from "@material-tailwind/react";
+import { HeartIcon } from "@heroicons/react/24/solid";
+
+export function Footer({ brandName, brandLink, routes }) {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="py-4 dark:bg-gray-900">
+      <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
+        <Typography variant="small" className="font-normal text-inherit">
+          &copy; {year}, made with{" "}
+          <HeartIcon className="-mt-0.5 inline-block text-red-600 w-6" /> by{" "}
+          <a
+            href={brandLink}
+            target="_blank"
+            className="transition-colors hover:text-blue-500"
+          >
+            {brandName}
+          </a>{" "}
+          for a better web.
+        </Typography>
+        <ul className="flex items-center gap-4">
+          {routes.map(({ name, path }) => (
+            <li key={name}>
+              <Typography
+                as="a"
+                href={path}
+                target="_blank"
+                variant="small"
+                className="py-0.5 px-1 font-normal text-inherit transition-colors hover:text-blue-500"
+              >
+                {name}
+              </Typography>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+Footer.defaultProps = {
+  brandName: "DEMBA SOW",
+  brandLink: "https://dembasow98.github.io",
+  routes: [
+    { name: "Demba Sow", path: "https://dembasow98.github.io" },
+    { name: "Website", path: "https://www.masterclassemlak.com" },
+    { name: "License", path: "https://www.creative-tim.com/license" },
+  ],
+};
+
+Footer.propTypes = {
+  brandName: PropTypes.string,
+  brandLink: PropTypes.string,
+  routes: PropTypes.arrayOf(PropTypes.object),
+};
+
+Footer.displayName = "/src/widgets/layout/footer.jsx";
+
+export default Footer;
